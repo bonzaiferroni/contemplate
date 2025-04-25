bafa107198c11bb127f3b8888f887555279274f5
Luke Bollwerk <lukas8@gmail.com>
2025-04-25 08:41:41 -0600

    sync db initialization

--- server/src/main/kotlin/ponder/contemplate/server/plugins/Databases.kt
+++ server/src/main/kotlin/ponder/contemplate/server/plugins/Databases.kt
@@ -1,10 +1,24 @@
 package ponder.contemplate.server.plugins
 
 import io.ktor.server.application.*
+import klutch.db.initDb
+import klutch.db.tables.RefreshTokenTable
+import klutch.db.tables.UserTable
 import klutch.environment.readEnvFromPath
+import ponder.contemplate.server.db.tables.ExampleTable
 
 fun Application.configureDatabases() {
-    initDb(env)
+    initDb(env, dbTables)
 }
 
 val env = readEnvFromPath()
+
+val dbTables = listOf(
+    UserTable,
+    RefreshTokenTable,
+    ExampleTable,
+)
+
+//CREATE DATABASE example_db;
+//CREATE USER example_user WITH PASSWORD 'hunter2';
+//GRANT ALL PRIVILEGES ON DATABASE example_db TO example_user;
